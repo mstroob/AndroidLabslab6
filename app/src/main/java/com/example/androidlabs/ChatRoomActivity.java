f35e78293455c749c17a2b59dc2d5e241c5c5d7b
Mark <markstroob@gmail.com>
2020-03-09 19:58:21 -0400

    lab4

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -6,47 +6,98 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import java.util.ArrayList;
+import android.app.AlertDialog;
 
 public class ChatRoomActivity extends AppCompatActivity {
 
-    private Context context = null;
+    public ArrayList<Message> messageList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
-
         ListView myList = (ListView) findViewById(R.id.listView);
-
-        View chatFooter =  ((LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.chat_room_footer, null, false);
-        myList.addFooterView(chatFooter);
-
-
-
         MyListAdapter adapter = new MyListAdapter();
-        myList.setAdapter( adapter );
+        myList.setAdapter(adapter);
+
+
+
+
 
 
 
         Button sendButton = (Button)findViewById(R.id.sendButton);
         sendButton.setOnClickListener ( bt -> {
-            EditText editText3 = (EditText) findViewById(R.id.editText3);
-            Message sentMessage = new Message(editText3.getText().toString(),true);
-           // myList.notifyDatasetChanged();
-        });
-        Button recieveButton = (Button)findViewById(R.id.sendButton);
-        sendButton.setOnClickListener ( bt -> {
-            EditText editText3 = (EditText) findViewById(R.id.editText3);
-            Message recieveMessage = new Message(editText3.getText().toString(),false);
-            //myList.notifyDatasetChanged();
-
+            EditText chatText = (EditText) findViewById(R.id.chatText);
+            messageList.add( new Message(chatText.getText().toString(),true));
+            adapter.notifyDataSetChanged();
         });
 
+        Button recieveButton = (Button)findViewById(R.id.recieveButton);
+        recieveButton.setOnClickListener ( bt -> {
+            EditText chatText = (EditText) findViewById(R.id.chatText);
+            messageList.add( new Message(chatText.getText().toString(),false));
+            adapter.notifyDataSetChanged();
 
+        });
+
+        myList.setOnItemLongClickListener (new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,int pos, long id) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ChatRoomActivity.this);
+                alertDialogBuilder.setMessage("Do you want to delete this? \n The selected row is: " + pos + "\n The database id is: " + "null");
+                alertDialogBuilder.setPositiveButton("Delete", (click, arg) -> {
+                    messageList.remove(pos);
+                    adapter.notifyDataSetChanged();
+                });
+                alertDialogBuilder.setNegativeButton("Cancel", (click, arg) -> {
+                });
+                alertDialogBuilder.show();
+                return true;
+            }
+        });
     }
 
+    private class MyListAdapter extends BaseAdapter {
 
+        private MyListAdapter(){
+            super();
+        }
+
+        @Override
+        public int getCount() {
+            return (messageList.size());
+        }
+
+        @Override
+        public Object getItem(int position) {
+           return (messageList.get(position));
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View newView = new View(null);
+            if (messageList.get(position).isSent()) {
+                LayoutInflater inflater = getLayoutInflater();
+                newView = inflater.inflate(R.layout.send_layout, parent, false);
+            }else{
+                LayoutInflater inflater = getLayoutInflater();
+                newView = inflater.inflate(R.layout.send_layout, parent, false);
+            }
+            return newView;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+    }
 
 }
